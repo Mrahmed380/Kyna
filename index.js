e03e13ce955f09d910313a7f52ad52406408406a
Kyna <coder.lilbloody@gmail.com>
2020-06-22 14:18:11 +0900

    Kyna

---
+++ index.js
@@ -0,0 +1,106 @@
+const Discord = require('discord.js');
+const client = new Discord.Client()
+const { Client, MessageEmbed } = require("discord.js");
+
+client.on('ready', () => {
+    console.log('(っ◔◡◔)っ ♥ Kyna ♥');
+    client.user.setActivity("k/help");
+  });
+
+  client.on("message", message => {
+    if (!message.guild) return;
+  
+    if (message.content.startsWith("k/kick")) {
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member
+            .kick("Optional reason that will display in the audit logs")
+            .then(() => {
+              message.reply(`Successfully kicked ${user.tag}`);
+            })
+            .catch(err => {
+              message.reply("I was unable to kick the member");
+              console.error(err);
+            });
+        } else {
+          message.reply("That user isn't in this guild!");
+        }
+      } else {
+        message.reply("You didn't mention the user to kick!");
+      }
+    }
+  });
+  
+  client.on("message", message => {
+    if (!message.guild) return;
+  
+    if (message.content.startsWith("k/ben")) {
+      const user = message.mentions.users.first();
+      if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member
+            .ban({
+              reason: "They were bad!"
+            })
+            .then(() => {
+              message.reply(`Successfully banned ${user.tag}`);
+            })
+            .catch(err => {
+              message.reply("I was unable to ban the member");
+              console.error(err);
+            });
+        } else {
+          message.reply("That user isn't in this guild!");
+        }
+      } else {
+        message.reply("You didn't mention the user to ban!");
+      }
+    }
+  });
+
+  client.on("message", message => {
+    if (message.content === "!react") {
+      message.react("😄");
+    }
+  });
+
+  client.on("message", function(message) {
+    if (message.content === "카이나 정보") {
+      message.channel.send("사는곳: Republic of Korea");
+      message.channel.send("취미: 노래 듣기");
+      message.channel.send("제작자: Lil_Bloody");
+    }
+  });
+
+  client.on("message", message => {
+    if (message.content === "k/di Lil_Bloody") {
+      const embed = new MessageEmbed()
+        .setTitle("**Dveloper Lil_Bloody information**")
+        .setColor(0xf8a5ff)
+        .setThumbnail(
+          "https://cdn.discordapp.com/attachments/644776656203939850/723703299227910174/unknown.png"
+        )
+        .addField("**Where to live** ", "[Korea]")
+        .addField("**Birthday** ", "[April 6]")
+        .addField("**Team** ", "[Kyna TM]")
+        .addField("**Kyna Support server rank** ", "[Owner]")
+        .addField(
+          "**Twitter:**",
+          "[LilBloody_Twitter](https://twitter.com/LilBloody_)",
+          true
+        )
+        .addField(
+            "**Twitter:**",
+            "[Kyna_Twitter](https://twitter.com/KynaDiscord)",
+            true
+          )
+        .setFooter("Can I help? | k/help", "https://cdn.discordapp.com/attachments/644776656203939850/723703299227910174/unknown.png");
+      message.channel.send(embed);
+    }
+  });
+  
+
+client.login(process.env.token);
