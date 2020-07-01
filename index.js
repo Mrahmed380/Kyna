b2e9b4956ab37bd7a01f236a48120306e77832a9
Kyna <coder.lilbloody@gmail.com>
2020-07-01 09:22:54 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
-const client = new Discord.Client()
+const client = new Discord.Client();
+const bot = new Discord.Client();
 const { Client, MessageEmbed } = require("discord.js");
 
 client.on('ready', () => {
@@ -93,5 +94,28 @@ client.on('ready', () => {
       message.channel.send(embed);
     }
   });
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    const messageArray = message.content.split(' ');
+	const cmd = messageArray[0];
+	const args = messageArray.slice(1);
+
+    if (cmd === '?poll'){
+        let pollChannel = message.mentions.channels.first();
+        let pollDescription = args.slice(1).join(' ');
+
+        let embedPoll = new Discord.MessageEmbed()
+        .setTitle('😲 New Poll! 😲')
+        .setDescription(pollDescription)
+        .setColor('YELLOW')
+        let msgEmbed = await pollChannel.send(embedPoll);
+        await msgEmbed.react('👍')
+        await msgEmbed.react('👎')
+    }
+
+})
+
 
 client.login(process.env.token);
