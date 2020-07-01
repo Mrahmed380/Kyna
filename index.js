50e73aecbad1be57a6c7d457a2a270daf48fdad3
Kyna <coder.lilbloody@gmail.com>
2020-07-01 09:49:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const request = require('request-promise');
-const cheerio = require('cheerio');
 const { Client, MessageEmbed } = require("discord.js");
 
 client.on('ready', () => {
