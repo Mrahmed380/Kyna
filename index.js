b983e805f7abb99451331a9e99cd43a2d5841701
Kyna <coder.lilbloody@gmail.com>
2020-07-02 13:38:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
-const { Client, MessageEmbed } = require("discord.js");
 
 client.on('ready', () => {
     console.log('(っ◔◡◔)っ ♥ Kyna ♥');
