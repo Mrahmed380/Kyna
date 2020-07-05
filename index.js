a06f57d90b9c18e2517846a9c42aea3e90a5a9e3
Kyna <coder.lilbloody@gmail.com>
2020-07-05 19:47:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -153,27 +153,4 @@ client.on("message", async message => {
 
 })
 
- client.on("message", message => {
-    if (message.content === "k/embed") {
-      const embed = new MessageEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
-      message.channel.send(embed);
-    }
-  });
-
 client.login(process.env.token);
