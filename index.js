f15a973a9d7db55d46feae77ba6bf16c174c1559
Kyna <coder.lilbloody@gmail.com>
2020-07-06 08:52:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -153,4 +153,34 @@ client.on("message", async message => {
 
 })
 
+bot.on('message', message => {
+    if (message.content.toLowerCase() === '!test'){
+        let filter = m => m.author.id === message.author.id;
+        let q1 = new Discord.MessageCollector(message.channel, filter, {
+            max: 1
+        })
+        message.channel.send('Which Channel you want me to send');
+
+        q1.on('collect', async (message, col) => {
+            let channel = message.mentions.channels.first();
+
+            message.channel.send('What is the message you want to send in there?')
+            q1.stop();
+            let q2 = new Discord.MessageCollector(message.channel, filter, {
+                max: 1
+            })
+            q2.on('collect', async (message, col) => {
+                channel.send(message.content);
+                await message.react('😀');
+                message.channel.send(`Its working! go to ${channel} to check your message out!!`)
+                q2.stop();
+            })
+        })
+
+
+    }
+})
+
+
+
 client.login(process.env.token);
