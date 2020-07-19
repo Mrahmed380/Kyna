1dd5b98489a6a3673a034504f5137673ec0a71bd
StelliDev <coder.lilbloody@gmail.com>
2020-07-19 10:30:56 +0900

    Update index.js

--- index.js
+++ index.js
@@ -198,4 +198,68 @@ client.on('message', message => {
 })
 
 
+client.on('message', async message => {
+  if (message.author.bot) return;
+  let prefix = 'k/';
+  if (!message.content.startsWith(prefix)) return;
+  
+  let args = message.content.slice(prefix.length).trim().split(/ +/g);
+  let msg = message.content.toLowerCase();
+  let cmd = args.shift().toLowerCase();
+  
+  if (msg.startsWith(prefix + 'stats')) {
+    cpuStat.usagePercent(function (error, percent, seconds) {
+      if (error) {
+        return console.error(error)
+      }
+      
+      const cores = os.cpus().length
+      const cpuModel = os.cpus()[0].model
+      const guild = client.guilds.cache.size.toLocaleString()
+      const user = client.users.cache.size.toLocaleString()
+      const channel = client.channels.cache.size.toLocaleString()
+      const usage = formatBytes(process.memoryUsage().heapUsed)
+      const Node = process.version
+      const CPU = percent.toFixed(2)
+      
+      const embed = new Discord.MessageEmbed()
+      embed.addField('Bot Statistics:', `Server: ${guild} \nUser: ${user} \nChannel: ${channel} \nUsage: ${usage} \nNode: ${Node} \nCPU Usage: ${CPU}%`)
+      embed.addField('Physical Statistics:', `CPU: ${cores} - ${cpuModel} \nUptime: **${parseDur(client.uptime)}**`)
+      message.channel.send(embed)
+    })
+  }
+});
+
+function formatBytes (a, b) {
+  if (0 == a) return "0 Bytes";
+  let c = 1024,
+      d = b || 2,
+      e = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+      f = Math.floor(Math.log(a) / Math.log(c));
+  
+  return parseFloat((a / Math.pow(c, f)).toFixed(d)) + " " + e[f]
+}
+
+function parseDur(ms) {
+  let seconds = ms / 1000,
+      days = parseInt(seconds / 86400);
+  seconds = seconds % 86400
+  
+  let hours = parseInt(seconds / 3600);
+  seconds = seconds % 3600
+  
+  let minutes = parseInt(seconds / 60);
+  seconds = parseInt(seconds % 60)
+  
+  if (days) {
+    return `${days} day, ${hours} hours, ${minutes} minutes`
+  } else if (hours) {
+    return `${hours} hours, ${minutes} minutes, ${seconds} seconds`
+  } else if (minutes) {
+    return `${minutes} minutes, ${seconds} seconds`
+  }
+  
+  return `${seconds} second(s)`
+}
+
 client.login(process.env.token);
